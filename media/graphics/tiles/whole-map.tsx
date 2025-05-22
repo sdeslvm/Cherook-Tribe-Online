1974e1fc5ef5dec40a67510bd2260081e435d0ef
uqukina <a1@MacBook-Air-1.local>
2025-05-22 14:21:18 +0300

    commit changes1

---
+++ media/graphics/tiles/whole-map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="whole map" tilewidth="117" tileheight="160" tilecount="28" columns="4">
+ <image source="tiles.png" width="468" height="1120"/>
+</tileset>
